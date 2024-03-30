81aa70299ba3cf4047ffc54380e6f539eae668d8
Marlon Menezes <marlon_menezes@outlook.com>
2024-03-30 19:28:37 -0300

    Feat: Project started

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export  default function Page404(){
+  return (
+    <div>
+      <h1>Ops... Página não encontrada. 😞</h1>
+      <Link href="/">
+        Ir para Home
+      </Link>
+    </div>
+  )
+}
