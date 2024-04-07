b7da2089129450431e1f9be4c8730cfa01a2ccb0
Marlon Menezes <marlon_menezes@outlook.com>
2024-04-07 20:36:21 -0300

    Feat: End of project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-// import { Head } from "next/document";
 import Script from "next/script";
 import GlobalStyle from "../src/theme/GlobalStyle";
 
@@ -8,12 +7,9 @@ function MyApp({ Component, pageProps }) {
     <>
         {/* <!-- Google tag (gtag.js) --> */}
         <Script
-          // strategy="lazyOnload"
           async src={`https://www.googletagmanager.com/gtag/js?id=G-0MRGDLEBLG`}
         />
-        <Script
-          // strategy="lazyOnload"
-          // id="gtag"
+        <Script          
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -23,18 +19,6 @@ function MyApp({ Component, pageProps }) {
             `
           }}
         />
-
-        {/* Head  */}
-
-        {/* <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="description" content="Projeto desenvolvido durante o curso de Next.js da Alura" />
-          <meta property="og:type" content="website" />
-          <meta property="og:locale" content="pt_BR" />
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
-        </Head> */}
         
       <Head>        
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
